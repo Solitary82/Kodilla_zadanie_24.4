c04f23bd1bb144c37027b897545bd0aca6214d47
Marta Waplak <mwaplak@gmail.com>
2018-06-06 00:59:02 +0200

    Add project

---
+++ index.js
@@ -0,0 +1,12 @@
+function checkZero(number, value) {
+  return number ? `${number}${value} ` : ''
+}
+
+const formatDate = sec => {
+  const hour = Math.floor(sec / 3600);
+  const minute = Math.floor(sec % 3600 / 60);
+  const second = (sec % 60);
+  return !sec ? '0s' : `${checkZero(hour, 'h')}${checkZero(minute, 'm')}${checkZero(second, 's')}`.trim();
+}
+
+module.exports = formatDate;
